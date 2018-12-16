7e0868a7f0f4ee73100cb50d564d6db08339753b
Andrew Duncan <duncanaj2@gmail.com>
2018-12-16 19:59:42 +0000

    Adding tinytex for tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,12 @@
 library(testthat)
 library(thesisdownrli)
 
+# installing tinytex if not already present
+if (!("tinytex" %in% installed.packages())) {
+  install.packages("tinytex")
+  tinytex::install_tinytex(force = TRUE)
+} else if (!tinytex:::is_tinytex()) {
+  tinytex::install_tinytex(force = TRUE)
+}
+
 test_check("thesisdownrli")
